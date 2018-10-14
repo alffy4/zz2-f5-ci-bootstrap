aa8d45fe7476199c432c8890bb9d38598a270a76
Julien Usson <usson.julien@gmail.com>
2018-10-14 16:40:50 +0200

    tests

--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ app.use("/static", express.static("public"));
 app.use("/", indexRouter);
 
 // Start app on port APP_PORT
-app.listen(process.env.APP_PORT || 3000, () =>
+export const server = app.listen(process.env.APP_PORT || 3000, () =>
   console.log(`App listening on port ${process.env.APP_PORT}!`)
 );
+
+export default app;
